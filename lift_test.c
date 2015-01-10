b31505881d1b41853cbbd068d21e452263086a18
Kenneth Li <kennethli.3470@gmail.com>
2015-01-09 20:03:47 -0500

    Created lift_test.c.

---
+++ lift_test.c
@@ -0,0 +1,34 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTMotor)
+#pragma config(Hubs,  S2, HTServo,  none,     none,     none)
+#pragma config(Sensor, S3,     HTSPB,          sensorI2CMuxController)
+#pragma config(Motor,  mtr_S1_C1_1,     driveFL,       tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     driveBL,       tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     lift1,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_2,     lift2,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     acquirer,      tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_2,     blankMotor,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C4_1,     driveFR,       tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C4_2,     driveBR,       tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S2_C1_1,    goalClamp,            tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_2,    liftBox,              tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_3,    servo3,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_6,    servo6,               tServoNone)
+
+
+task main()
+{
+	while (true) {
+		if (nNxtButtonPressed = 1) {
+			motor[lift1] = 100;
+			motor[lift2] = 100;
+		} else if (nNxtButtonPressed = 2) {
+			motor[lift1] = -100;
+			motor[lift2] = -100;
+		} else {
+			motor[lift1] = 0;
+			motor[lift2] = 0;
+		}
+	}
+}
